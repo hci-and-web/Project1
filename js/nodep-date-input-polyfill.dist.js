e3cb18dfe8bca1d9cde7915e005110a02a4fa209
Hannah Leland <hannahleland@users.noreply.github.com>
2020-03-19 18:07:08 -0500

    text alighnment, spacing, and weight

--- js/nodep-date-input-polyfill.dist.js
+++ js/nodep-date-input-polyfill.dist.js
@@ -1,3 +1,10 @@
+/* 
+
+TODO
+This polyfill script ... 
+
+*/
+
 (function(a, b) {
   'object' == typeof exports && 'undefined' != typeof module ? b() : 'function' == typeof define && define.amd ? define(b) : b()
 })(this, function() {
